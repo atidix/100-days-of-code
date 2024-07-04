88918a8ac88a6dd778710a1957a7e19d58c9624f
atidix <aditi96dixit@gmail.com>
2024-07-04 13:21:06 +0530

    updated to add high score

--- day-20 & 21/main.py
+++ day-20 & 21/main.py
@@ -34,12 +34,12 @@ while game_on:
         scoreboard.update_score()
     
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
     
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_on = False
-            scoreboard.game_over()
+            snake.reset()
+            scoreboard.reset()
    
 screen.exitonclick()
--- day-20 & 21/scoreboard.py
+++ day-20 & 21/scoreboard.py
@@ -9,17 +9,28 @@ class Scoreboard(Turtle):
         super().__init__()
         self.color("white")
         self.score = 0
+        with open("./day-20 & 21/data.txt") as read:
+            self.high_score = int(read.read())
         self.penup()
         self.goto(SCORE_POSITION)
         self.hideturtle()
         self.write_score()
     
     def write_score(self):
-        self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, ALIGNMENT, FONT)
     
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", False, ALIGNMENT, FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open ("./day-20 & 21/data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.clear()
+        self.write_score()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", False, ALIGNMENT, FONT)
 
     def update_score(self):
         self.clear()
